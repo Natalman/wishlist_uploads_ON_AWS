88c53f99b131ddea6369283020e67d71b632a676
clara <clara@clara.com>
2017-03-05 22:27:08 -0600

    remove extra file

--- wishlist/travel_wishlist/signals.py
+++ wishlist/travel_wishlist/signals.py
@@ -2,9 +2,7 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from travel_wishlist.models import Place
 
-
 from django.core.files.storage import default_storage
-
 
 @receiver(post_delete, sender=Place)
 def place_post_delete_image_cleanup(sender, **kwargs):
@@ -13,7 +11,6 @@ def place_post_delete_image_cleanup(sender, **kwargs):
     # instance is the Place object.
     # The Place object has been deleted from the DB,
     # but still has data in its fields.
-
     if place.photo:
         if default_storage.exists(place.photo):
             default_storage.delete(place.photo)
@@ -31,4 +28,5 @@ def place_pre_save_image_cleanup(sender, **kwargs):
     # check to see if there is a photo, and if so, delete it
     if old_place and old_place.photo:
         if default_storage.exists(old_place.photo):
+            print('delete', old_place.photo)
             default_storage.delete(old_place.photo)
